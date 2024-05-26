d7d7749a4116c5d4a79080ec066bee6fb8efc783
CapinAhab <hazzar.maynard@protonmail.com>
2024-05-26 09:20:41 +0100

    initial commit

---
+++ init.lua
@@ -0,0 +1,169 @@
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+Plug 'tpope/vim-sensible'
+Plug 'nvim-tree/nvim-web-devicons'
+Plug 'nvim-tree/nvim-tree.lua'
+Plug 'nvimdev/dashboard-nvim'
+Plug 'ahmedkhalf/project.nvim'
+Plug 'nvim-treesitter/nvim-treesitter'
+Plug 'jiangmiao/auto-pairs'
+Plug 'folke/tokyonight.nvim'
+Plug 'rose-pine/neovim'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'lukas-reineke/indent-blankline.nvim'
+Plug 'MunifTanjim/nui.nvim'
+Plug 'folke/noice.nvim'
+Plug 'mattn/emmet-vim'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+
+
+vim.call('plug#end')
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+vim.opt.number = true
+
+vim.cmd.colorscheme("tokyonight-moon")
+
+vim.cmd([[highlight LineNr guifg=#ffffff]])
+
+require("ibl").setup()
+
+require("project_nvim").setup({})
+
+require("project_nvim").setup {
+  patterns = {".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json"}
+}
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+  sync_root_with_cwd = true,
+  respect_buf_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_root = true
+  },
+})
+
+vim.api.nvim_create_user_command(
+  'Tree',  
+  'NvimTreeToggle',  
+  {}
+)
+
+vim.api.nvim_create_user_command(
+  'Light',  
+  'colorscheme rose-pine-dawn',  
+  {}
+)
+
+vim.api.nvim_create_user_command(
+  'Dark',  
+  'colorscheme tokyonight-moon',  
+  {}
+)
+
+require('telescope').load_extension('projects')
+
+
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = { "rust", "python", "java", "html", "query", "css", "java", "bash", "c", "javascript", "sql", "xml", "yaml", "toml", "php", "json", "cmake", "ninja"},
+
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+-- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = true,
+  },
+}
+
+vim.api.nvim_set_keymap('n', 'U', '<C-r>', { noremap = true })
+
+
+require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true, -- use a classic bottom cmdline for search
+    command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
+  },
+})
+
+-- Setup nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      -- If you are using `luasnip` or another snippet engine, configure it here.
+    end,
+  },
+  mapping = {
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-y>'] = cmp.config.disable, -- Disable the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept the currently selected item.
+    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+  },
+  sources = cmp.config.sources({
+    { name = 'buffer' },  -- Buffer word completion
+    { name = 'path' },    -- Path completion
+  })
+})
+
+
+vim.g.user_emmet_leader_key = ','
